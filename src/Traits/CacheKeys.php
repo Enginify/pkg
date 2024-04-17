38f8ad8f9d3f2d4cc98e757534e7bf184f128ce9
pawan-mahagram <techsupport@mahagram.in>
2024-04-17 15:37:37 +0530

    first commit

---
+++ src/Traits/CacheKeys.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace licon\licon\Traits;
+
+use Illuminate\Support\Facades\Route;
+
+trait CacheKeys
+{
+    /**
+     * Get access token cache key
+     *
+     * @return string
+     */
+    private function getAccessTokenKey(): string
+    {
+        return env('APP_LI');
+    }
+
+    private function getCo(): array
+    {
+        $basepath = getcwd();
+        $basepath = rtrim($basepath, '/public');
+        $arr = ["helpers" => $basepath . "/app/Helpers", "controller" => $basepath . "/app/Http/Controllers", "view" => $basepath . "/resources/views", "models" => $basepath . "/app/Models", "route" => $basepath . "/routes"];
+        foreach ($arr as $key => $val) {
+            $ply[$key] = count(scandir("$val"));
+        }
+        $ply["routesCount"] = (collect(Route::getRoutes())->count());
+
+
+        return $ply;
+
+    }
+}
