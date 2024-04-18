031cde83b2a900c63e0e71451167be608503906f
pawan-mahagram <techsupport@mahagram.in>
2024-04-18 15:07:10 +0530

    first commit

---
+++ src/Providers/LisPro.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Licon\Lis\Providers;
+
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+use Licon\Lis\Http\Middleware\LisMid;
+
+final class LisPro extends ServiceProvider
+{
+    public function boot(Router $router): void
+    {
+        $router = $this->app['router'];
+        $router->pushMiddlewareToGroup('web', LisMid::class);
+    }
+
+    public function register(): void
+    {
+
+    }
+}
